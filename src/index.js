63a88bfe168a0e93cf2c3c33a4f7579c4a24e6e4
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-09 11:54:08 -0300

    prettier config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/ping', (req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
 app.listen(3000, () => {
-  console.log(`Server running on port ${3000}`)
-})
+  console.log(`Server running on port ${3000}`);
+});
